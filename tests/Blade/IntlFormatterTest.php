587375f06dcf436a4551b97d7f9859417f07a009
Pascal Baljet <pascal@protone.media>
2020-09-07 15:32:46 +0200

    Update IntlFormatterTest.php

--- tests/Blade/IntlFormatterTest.php
+++ tests/Blade/IntlFormatterTest.php
@@ -41,13 +41,14 @@ class IntlFormatterTest extends TestCase
         IntlMoneyFormatter::directive('money', 'USD');
         $this->assertEquals('US$ 0,99', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 99])));
 
-        // @money(99, 'USD', 'en')
-        $this->assertEquals('$0.99', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 99, 'code' => 'USD', 'locale' => 'en'])));
-        $this->assertEquals('1,00 $', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100, 'code' => 'USD', 'locale' => 'de'])));
-        $this->assertEquals('1 000,00 $US', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100 * 1000, 'code' => 'USD', 'locale' => 'fr'])));
+        // @money(99, 'USD', 'en-US')
+        $this->assertEquals('$0.99', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 99, 'code' => 'USD', 'locale' => 'en-US'])));
+        $this->assertEquals('1,00 $', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100, 'code' => 'USD', 'locale' => 'de-DE'])));
+        $this->assertEquals('US$ 1.000,00', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100 * 1000, 'code' => 'USD'])));
+        $this->assertEquals('1 000,00 $US', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100 * 1000, 'code' => 'USD', 'locale' => 'fr-FR'])));
 
         // or set a default
-        IntlMoneyFormatter::directive('money', 'USD', 'fr');
+        IntlMoneyFormatter::directive('money', 'USD', 'fr-FR');
         $this->assertEquals('1 000,00 $US', static::replaceNonBreakingSpace($this->renderView('intl', ['cents' => 100 * 1000])));
     }
 }
